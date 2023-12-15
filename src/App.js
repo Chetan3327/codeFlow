8b576e98bf865195a7e228727a6001eb15c5e9b2
Chetan3327 <chauhanchetan12789@gmail.com>
2023-12-15 21:25:48 +0530

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='flex items-center justify-center min-h-screen text-4xl font-bold  text-slate-700 bg-cyan-200'>
+      hello
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
